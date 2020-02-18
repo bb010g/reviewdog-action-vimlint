a4c8e708fd80dbb6a4c3bd3513ed5b26f621c88e
Dusk <me@bb010g.com>
2020-02-17 22:04:44 -0800

    Initial vimlint commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
-set -e
+#!/bin/ash
+# shellcheck shell=sh
+# shellcheck disable=SC2039
+set -eo pipefail
 
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
@@ -7,5 +9,39 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" -name="linter-name (misspell)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+tolower() {
+  tr '[:upper:]' '[:lower]'
+}
+tobool() {
+  str=$(printf '%s\n' "$2" | tolower)
+  if [ 'xtrue' = "x$str" ]; then
+    printf 'y\n'
+  elif [ 'xfalse' = "x$str" ]; then
+    printf 'n\n'
+  else
+    printf 'invalid boolean %s: %s\n' "$1" "$str" >&2 && return 2
+  fi
+}
+bool_status() {
+  bool=$(tobool "$@")
+  rc=$?
+  [ "$rc" != 0 ] && return "$rc"
+  [ "$bool" = 'y' ] && return 0
+  return 1
+}
+
+if bool_status 'input_verbose' "${INPUT_VERBOSE}"; then
+  INPUT_FLAGS="-v ${INPUT_FLAGS}"
+fi
+if bool_status 'only_error' "${INPUT_ONLY_ERROR}"; then
+  INPUT_FLAGS="-E ${INPUT_FLAGS}"
+fi
+
+# shellcheck disable=SC2086
+sh /opt/vim/vimlint/bin/vimlint.sh -l /opt/vim/vimlint -p /opt/vim/vimlparser \
+    ${INPUT_FLAGS} ${INPUT_PATH:+"${INPUT_PATH}"} ${INPUT_PATHS} . \
+  | reviewdog -name="vimlint" \
+    -efm="%f:%l:%c:%trror: %m" -efm="%f:%l:%c:%tarning: %m" -efm="%f:%l:%c:%m" \
+    -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+
+# vim:et:sw=2:sts=2:sta:tw=0:ff=unix
